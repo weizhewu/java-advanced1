f55a52088192435d44d3b31b5349fdad76de87b3
weizhewu <weizhewu232623@gmail.com>
2019-04-02 07:52:33 +0800

    CardLayoutTest

--- collection-study/src/com/soft1841/punchTheClock/sixthDay/Test.java
+++ collection-study/src/com/soft1841/punchTheClock/sixthDay/Test.java
@@ -1,4 +1,37 @@
 package com.soft1841.punchTheClock.sixthDay;
 
+import java.io.File;
+import java.io.ObjectStreamException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
+    public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
+        // 遍历文件
+        String path = "D:/img";
+        File file = new File(path);
+        if (file.exists() ) {
+            List<File> fs = new ArrayList<>();
+            getAllFile(file, fs);
+            System.out.println("文件夹下的图片有：");
+            for (File f : fs) {
+                System.out.println(f.getName());
+            }
+        }
+    }
+
+    private static void getAllFile(File mFile, List<File> f) {
+        // 获取子目录
+        File[] files = mFile.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                getAllFile(file, f);
+            } else {
+                String fileName = file.getName();
+                if (fileName.endsWith(".jpg") || (fileName.endsWith(".png"))) {
+                    f.add(file);
+                }
+            }
+        }
+    }
 }
--- collection-study/src/com/soft1841/punchTheClock/sixthDay/Test1.java
+++ collection-study/src/com/soft1841/punchTheClock/sixthDay/Test1.java
@@ -1,4 +1,32 @@
 package com.soft1841.punchTheClock.sixthDay;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test1 {
+    private static List<File> getData(String path) {
+        List<File> list = new ArrayList<>();
+        try {
+            File f = new File(path);
+            File[] fs = f.listFiles();
+            int i = 0;
+            while (i<f.length()) {
+                if (fs[i].isDirectory())
+                    getData(fs[i].getPath());// 递归查询目录下的文件夹
+                if (fs[i].getName().endsWith(".jpg"))// 查找xls后缀的文件
+                    list.add(fs[i]);
+                i++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        List<File> list = getData("D:\\img");
+        for (File f : list) {
+            System.out.println(f.getName());
+        }
+    }
 }
