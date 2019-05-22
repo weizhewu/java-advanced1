9d566bcf2d900592f7fae9f9e7f06b7c03254ad4
weizhewu <weizhewu232623@gmail.com>
2019-05-23 06:53:22 +0800

    Java设计模式四

---
+++ java-manage/src/com/soft1841/fourthDay/Proxy.java
@@ -0,0 +1,18 @@
+package com.soft1841.fourthDay;
+
+public class Proxy implements Subject {
+    @Override
+    public void buyMac() {
+        //引用并创建真实对象实例1，即“我”
+        RealSubject realSubject = new RealSubject();
+
+        //调用真实对象的方法，进行代理购买Mac
+        realSubject.buyMac();
+        //代理对象额外做的操作
+        this.WrapMac();
+    }
+    public void WrapMac(){
+        System.out.println("用盒子包装盒号Mac");
+    }
+
+}
---
+++ java-manage/src/com/soft1841/fourthDay/Subject.java
@@ -0,0 +1,5 @@
+package com.soft1841.fourthDay;
+
+public interface Subject {
+    public void buyMac();
+}
