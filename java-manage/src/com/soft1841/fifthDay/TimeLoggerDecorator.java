6487f26122a91ce5e8fb6d861f39f1f1801a7f97
weizhewu <weizhewu232623@gmail.com>
2019-05-24 01:18:45 +0800

    Java设计模式五

---
+++ java-manage/src/com/soft1841/fifthDay/TimeLoggerDecorator.java
@@ -0,0 +1,20 @@
+package com.soft1841.fifthDay;
+
+import java.util.Calendar;
+
+public class TimeLoggerDecorator implements Logger {
+    private Logger decoratorLogger;
+
+    public TimeLoggerDecorator(Logger decoratorLogger){
+        this.decoratorLogger=decoratorLogger;
+    }
+
+    @Override
+    public void log(String message){
+        decoratorLogger.log(message);
+        logTime();
+    }
+    public void logTime(){
+        System.out.println(Calendar.getInstance().getTime());
+    }
+}
